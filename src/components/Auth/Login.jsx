a887903d842d3c63b8a2bae730092ca225ef07cc
kanavmahajan1 <220106074@hbtu.ac.in>
2025-08-04 21:26:25 +0530

    amin

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -18,7 +18,15 @@ const Login = ({handleLogin}) => {
 
   return (
     <div className='flex h-screen w-screen items-center justify-center'>
-        <div className='border-2 rounded-xl border-emerald-600 p-20'>
+      <div className='absolute top-10 left-1/2 -translate-x-1/2 text-center'>
+        <h1 className='text-3xl font-bold text-emerald-700 mb-2'>Made with 💖 by Kanav Gupta</h1>
+        <p className='text-lg text-gray-700'>Central Login Portal System </p>
+        <div className='bg-emerald-50 border border-emerald-300 rounded-lg px-4 py-2 inline-block mt-1 shadow'>
+          <span className='block text-emerald-800 font-semibold'>Email: <span className='font-mono'>e@e.com || admin@me.com</span></span>
+          <span className='block text-emerald-800 font-semibold'>Password: <span className='font-mono'>123 || 123</span></span>
+        </div>
+      </div>
+      <div className='border-2 rounded-xl border-emerald-600 p-20'>
             <form 
             onSubmit={(e)=>{
                 submitHandler(e)
